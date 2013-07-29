09c5d7044e98f18b7cc92c6cc1699dac5db9a94d
ilyakh <ilyakh@ulrik.uio.no>
2013-07-29 08:43:33 +0200

    allows two different methods of output: one as pwm at half the voltage, the other as usual digitalWrite command; added a 10K resistor

--- arduino_trigger.ino
+++ arduino_trigger.ino
@@ -3,19 +3,50 @@
 #define VOLTAGE_LOW_LEVEL 0
 #define VOLTAGE_HIGH_LEVEL 128
 
+#define INTERVAL 40
+#define SERIAL_BAUDRATE 9600
 
-#define INTERVAL 3000
+
+byte command;
+
 
 void setup() {
+  
  pinMode( TARGET_PIN_NR, OUTPUT ); 
+ Serial.begin( SERIAL_BAUDRATE );
+ 
+ clearSerialBuffer();
+ 
 }
 
+
 void loop() {
-  analogWrite( TARGET_PIN_NR, VOLTAGE_HIGH_LEVEL );
   
-  delay( INTERVAL );
-  
-  analogWrite( TARGET_PIN_NR, VOLTAGE_LOW_LEVEL );
-  
-  delay( INTERVAL );
+  if ( Serial.available() > 0 ) { 
+    
+    command = Serial.read();
+    
+    if ( command == 'A' ) {
+      // pwm signal at lower voltage
+      analogWrite( TARGET_PIN_NR, VOLTAGE_HIGH_LEVEL );
+      delay( INTERVAL );
+      analogWrite( TARGET_PIN_NR, VOLTAGE_LOW_LEVEL );
+    
+    } else {
+      // digital signal at max voltage
+      digitalWrite( TARGET_PIN_NR, HIGH );
+      delay( INTERVAL );
+      digitalWrite( TARGET_PIN_NR, LOW );  
+    }
+    
+    clearSerialBuffer(); // clears all commands received while the trigger was on
+    
+  }
+}
+
+
+void clearSerialBuffer() {
+ while( Serial.available() > 0 ) {
+   Serial.read(); 
+ }
 }
